245b222a3941f27b61c1c1cd2f8c46bc8f7935b3
Kat3rinka <66567976+Kat3rinka@users.noreply.github.com>
2020-09-08 19:31:22 +0300

    commented time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -2,6 +2,6 @@ import time
 
 def test_add_to_basket_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    time.sleep(30)
+    #time.sleep(30)
     assert browser.find_element_by_class_name("btn-add-to-basket").is_displayed()
     
